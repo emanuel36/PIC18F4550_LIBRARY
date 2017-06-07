0e2a58642723db648faad29a72750ba0f4dc77c3
emanuel <emanuel@Aspire-F5>
2017-06-07 20:26:54 -0300

    bug fix

--- inc/main.h
+++ inc/main.h
@@ -33,7 +33,7 @@
 
 // CONFIG4L
 #pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config LVP = OFF         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
 #pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
 #pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
 
--- src/main.c
+++ src/main.c
@@ -4,22 +4,16 @@
 #include <time.h>
 #include <string.h>
 #include <pic18f4550.h>
+#include "../inc/main.h"
 #include "../inc/lcd16x2.h"
 #include "../inc/tmr0Interrupt.h"
 
 #define _XTAL_FREQ  8000000
 
 int main(){
-    initLCD();
-    writeLCD("PIC18F4550");
-    selectLine(2, 0);
-    writeLCD("QUIXADA");
-    
-    TRISD = 0;
-    PORTD = 0;
+    TRISD = 0x00;
+    PORTD = 0x00;
     config_tmr0_interupt();
-    
     while(1);
-    
     return (EXIT_SUCCESS);
 }
