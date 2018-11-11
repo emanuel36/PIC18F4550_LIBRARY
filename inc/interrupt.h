1117d249ea6f45c48f55dc3fd22e77c458a59938
Emanuel Angelim <emanuel_ec@alu.ufc.br>
2018-11-11 13:40:56 -0200

    Revert "Change lcd to 4-bit mode and PWM corretions"
    This reverts commit 41fe4f40daef29f6c90f6b0c7dec0e259c12fb04.

--- inc/interrupt.h
+++ inc/interrupt.h
@@ -1,67 +1,13 @@
 #ifndef INTERRUPT_H
 #define	INTERRUPT_H
     
-#include <stdio.h>
-#include <stdlib.h>
 #include <xc.h>
 #include <pic18f4550.h>
+#define _XTAL_FREQ 16000000
 
 void config_tmr0();
 void config_int0();
 void config_int1();
 void config_int2();
-
-void config_tmr0(){
-    INTCONbits.PEIE_GIEL = 1;
-    T0CON = 0x00;
-    T0CONbits.TMR0ON = 1;
-    T0CONbits.T0PS = 3;
-    INTCONbits.TMR0IE = 1;
-    INTCONbits.GIE_GIEH = 1;
-}
-
-void config_int0(){
-    INTCONbits.PEIE_GIEL = 0;
-    TRISBbits.RB0 = 1;
-    INTCON2bits.INTEDG0 = 0;
-    INTCONbits.INT0IF = 0;
-    INTCONbits.INT0IE = 1;
-    INTCONbits.GIE_GIEH = 1;
-}
-
-void config_int1(){
-    INTCONbits.GIE_GIEH = 1;
-    INTCONbits.PEIE_GIEL = 0;
-    TRISBbits.RB1 = 1;
-    INTCON3bits.INT1IE = 1;
-    INTCON2bits.INTEDG2 = 0;
-    INTCON3bits.INT2IF = 0;
-}
-
-void config_int2(){
-    INTCONbits.GIE_GIEH = 1;
-    INTCONbits.PEIE_GIEL = 0;
-    TRISBbits.RB2 = 1;
-    INTCON3bits.INT2E = 1;
-    INTCON2bits.INTEDG2 = 0;
-    INTCON3bits.INT2IF = 0;
-}
-
-void ISR(){
-    if(INTCONbits.INT0IF){      //Botão 1 pressionado
-        INTCONbits.INT0IF = 0;
-        interrupt_flag = interrupt_flag | INT0;
-    }
-    
-    if(INTCON3bits.INT1IF){     //Botão 2 pressionado    
-        INTCON3bits.INT1IF = 0;
-        interrupt_flag = interrupt_flag | INT1;
-    }
-    
-    if(INTCON3bits.INT2IF){     //Botão 3 pressionado
-        INTCON3bits.INT2F = 0; 
-        interrupt_flag = interrupt_flag | INT2;
-    } 
-}
 
 #endif	/* INTERRUPT_H */
--- inc/lcd16x2.h
+++ inc/lcd16x2.h
@@ -1,89 +1,22 @@
 #ifndef LCD16X2_H
 #define	LCD16X2_H
     
-#include <stdio.h>
-#include <string.h>
-#include <pic18f4550.h>
 #include <xc.h>
+#include <pic18f4550.h>
 
-#define RS LATB0                    /*PIN 0 of PORTB is assigned for RS Pin of LCD*/
-#define EN LATB1                    /*PIN 1 of PORTB is assigned for ENABLE Pin of LCD */
-#define ldata LATB                  /*PORTB(RB4-RB7) is assigned for LCD Data Output*/ 
-#define LCD_Port TRISB              /*define macros for PORTB Direction Register*/
-
-void LCD_Init();
-void LCD_Command(unsigned char cmd );
-void LCD_Char(unsigned char dat);
-void LCD_String(const char *msg);
-void LCD_String_xy(char row,char pos,const char *msg);
-
-void LCD_Init(){
-    LCD_Port = 0;                   /*PORT as Output Port*/
-    __delay_ms(15);                    /*15ms,16x2 LCD Power on delay*/
-    LCD_Command(0x02);              /*send for initialization of LCD 
-                                     *for nibble (4-bit) mode */
-    LCD_Command(0x28);              /*use 2 line and 
-                                     *initialize 5*8 matrix in (4-bit mode)*/
-	LCD_Command(0x01);              /*clear display screen*/
-    LCD_Command(0x0c);              /*display on cursor off*/
-	LCD_Command(0x06);              /*increment cursor (shift cursor to right)*/	   
-}
-
-void LCD_Command(unsigned char cmd ){
-	ldata = (ldata & 0x0f) |(0xF0 & cmd);   /*Send higher nibble of command first to PORT*/ 
-	RS = 0;                                 /*Command Register is selected i.e.RS=0*/ 
-	EN = 1;                                 /*High-to-low pulse on Enable pin to latch data*/ 
-	NOP();
-	EN = 0;
-	__delay_ms(1);
-    ldata = (ldata & 0x0f) | (cmd<<4);      /*Send lower nibble of command to PORT */
-	EN = 1;
-	NOP();
-	EN = 0;
-	__delay_ms(3);
-}
-
-
-void LCD_Char(unsigned char dat){
-	ldata = (ldata & 0x0f) | (0xF0 & dat);   /*Send higher nibble of data first to PORT*/
-	RS = 1;                                  /*Data Register is selected*/
-	EN = 1;                                  /*High-to-low pulse on Enable pin to latch data*/
-	NOP();
-	EN = 0;
-	__delay_ms(1);
-    ldata = (ldata & 0x0f) | (dat<<4);               /*Send lower nibble of data to PORT*/
-	EN = 1;                         /*High-to-low pulse on Enable pin to latch data*/
-	NOP();
-	EN = 0;
-	__delay_ms(3);
-}
-void LCD_String(const char *msg){
-	while((*msg)!=0)
-	{		
-	  LCD_Char(*msg);
-	  msg++;	
-    }
-}
-
-void LCD_String_xy(char row,char pos,const char *msg){
-    char location=0;
-    if(row<=1)
-    {
-        location=(0x80) | ((pos) & 0x0f);      /*Print message on 1st row and desired location*/
-        LCD_Command(location);
-    }
-    else
-    {
-        location=(0xC0) | ((pos) & 0x0f);      /*Print message on 2nd row and desired location*/
-        LCD_Command(location);    
-    }  
-    
-
-    LCD_String(msg);
-
-}
-void LCD_Clear(){
-   	LCD_Command(0x01);     /*clear display screen*/
-}                  /*Clear LCD Screen*/
+void enableLCD();
+void sendCMD(int value);
+void sendASCII(char letra);
+void writeLCD(char *frase);
+void shiftRight();
+void shiftLeft();
+void selectLine(int line, int position);
+void clear_lcd();
+void initLCD();
+void waitPassLCD();
+void passLCD();
+void digitoLCD();
+void wrongPassLCD(int tentativas);
+void correctPassLCD();
 
 #endif	/* LCD16X2_H */
