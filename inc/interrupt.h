ce1f744649c7a4f7583a278c9ba2e9b15bb19156
Emanuel Angelim <emanuel_ec@alu.ufc.br>
2018-11-23 09:54:18 -0300

    interrupt corretions

--- inc/interrupt.h
+++ inc/interrupt.h
@@ -1,67 +1,101 @@
 #ifndef INTERRUPT_H
-#define	INTERRUPT_H
+#define INTERRUPT_H
     
 #include <stdio.h>
 #include <stdlib.h>
 #include <xc.h>
 #include <pic18f4550.h>
+#include "configuration.h"
+#include "lcd16x2.h"
 
-void config_tmr0();
-void config_int0();
-void config_int1();
-void config_int2();
+#define INT0_FLAG 0x1
+#define INT1_FLAG 0x2
+#define INT2_FLAG 0x4
+#define TMR0_FLAG 0x8
+#define TMR1_FLAG 0x10
+#define TMR2_FLAG 0x20
+#define TMR3_FLAG 0x40
+
+void disable_ext_int(){
+    INTCONbits.INT0IE = 0;
+    INTCON3bits.INT1IE = 0;
+    INTCON3bits.INT2E = 0;
+}
+
+void enable_ext_int(){
+    INTCONbits.INT0IE = 1;
+    INTCON3bits.INT1IE = 1;
+    INTCON3bits.INT2E = 1;
+}
 
 void config_tmr0(){
-    INTCONbits.PEIE_GIEL = 1;
-    T0CON = 0x00;
-    T0CONbits.TMR0ON = 1;
-    T0CONbits.T0PS = 3;
-    INTCONbits.TMR0IE = 1;
-    INTCONbits.GIE_GIEH = 1;
+    T0CONbits.PSA = 1;
+    T0CONbits.T0SE = 0;
+    T0CONbits.T0CS = 1;
+    T0CONbits.T08BIT = 0;
+    TMR0 = 0x00;
+    T0CONbits.TMR0ON = 0;
+    INTCONbits.TMR0IE = 0;
+}
+
+void config_tmr1(){
+    T1CONbits.RD16 = 0;
+    T1CONbits.T1CKPS = 0;
+    T1CONbits.T1OSCEN = 0;
+    T1CONbits.TMR1CS = 0;
+    T1CONbits.TMR1ON = 1;
+    TMR1 = 0;
+    PIE1bits.TMR1IE = 1;
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
 }
 
 void config_int0(){
-    INTCONbits.PEIE_GIEL = 0;
     TRISBbits.RB0 = 1;
-    INTCON2bits.INTEDG0 = 0;
+    INTCON2bits.INTEDG0 = 1;
     INTCONbits.INT0IF = 0;
     INTCONbits.INT0IE = 1;
-    INTCONbits.GIE_GIEH = 1;
+    INTCONbits.GIE = 1;
 }
 
 void config_int1(){
-    INTCONbits.GIE_GIEH = 1;
-    INTCONbits.PEIE_GIEL = 0;
     TRISBbits.RB1 = 1;
     INTCON3bits.INT1IE = 1;
-    INTCON2bits.INTEDG2 = 0;
+    INTCON2bits.INTEDG2 = 1;
     INTCON3bits.INT2IF = 0;
+    INTCONbits.GIE = 1;
 }
 
 void config_int2(){
-    INTCONbits.GIE_GIEH = 1;
-    INTCONbits.PEIE_GIEL = 0;
     TRISBbits.RB2 = 1;
     INTCON3bits.INT2E = 1;
     INTCON2bits.INTEDG2 = 0;
     INTCON3bits.INT2IF = 0;
+    INTCONbits.GIE = 1;
 }
 
-void ISR(){
-    if(INTCONbits.INT0IF){      //Botão 1 pressionado
+void interrupt ISR(){
+    if(INTCONbits.INT0IF){      
         INTCONbits.INT0IF = 0;
-        interrupt_flag = interrupt_flag | INT0;
+        interrupt_flag = interrupt_flag | INT0_FLAG;
+        DutyPWM1 += 0.1;
     }
     
-    if(INTCON3bits.INT1IF){     //Botão 2 pressionado    
+    if(INTCON3bits.INT1IF){      
         INTCON3bits.INT1IF = 0;
-        interrupt_flag = interrupt_flag | INT1;
+        interrupt_flag = interrupt_flag | INT1_FLAG;
+        DutyPWM1 -= 0.1;
     }
     
-    if(INTCON3bits.INT2IF){     //Botão 3 pressionado
+    if(INTCON3bits.INT2IF){   
         INTCON3bits.INT2F = 0; 
-        interrupt_flag = interrupt_flag | INT2;
+        interrupt_flag = interrupt_flag | INT2_FLAG;
     } 
+    
+    if(PIR1bits.TMR1IF){
+        PIR1bits.TMR1IF = 0;
+        interrupt_flag = interrupt_flag | TMR1_FLAG;
+    }
 }
 
-#endif	/* INTERRUPT_H */
+#endif  /* INTERRUPT_H */
--- inc/pwm.h
+++ inc/pwm.h
@@ -1,12 +1,12 @@
 #ifndef PWM_H
-#define	PWM_H
+#define PWM_H
     
 #include <pic18f4550.h>
 #include <stdio.h>
 #include <math.h>
 
 #define MINTHR              8000
-#define RESOLUTION          488
+#define RESOLUTION          49
 
 #define InternalOsc_8MHz    8000000
 #define InternalOsc_4MHz    4000000
@@ -21,41 +21,22 @@
 #define Timer2Prescale_4    4
 #define Timer2Prescale_16   16
 
-//The frequency of both PWMs have to be the same.
-
-unsigned long frequencia_pwm;
-
-void PWM1_config();
-int setPWM1(unsigned long FPWM, float duty_cycle);
-void SetDutyCyclePWM1(float Duty_cycle, int Period);
-
-void PWM2_config();
-int setPWM2(unsigned long FPWM, float duty_cycle);
-void SetDutyCyclePWM2(float Duty_cycle, int Period);
-
-void PWM1_config(){         
-    TRISCbits.TRISC2 = 0; 
+void PWM1_Init(){            
+    TRISCbits.TRISC2 = 0;  
     CCP1CON = 0x0C;       
 }
 
-void PWM2_config(){         
-    TRISCbits.TRISC1 = 0; 
-    CCP2CON = 0x0C;       
+void PWM2_Init(){            
+    TRISCbits.TRISC1 = 0;  
+    CCP2CON = 0x0C;        
 }
 
-int setPWM1(unsigned long FPWM, float duty_cycle){
+int setPeriodTo(unsigned long FPWM){/* Set period */
+
     int clockSelectBits, TimerPrescaleBits;
     int TimerPrescaleValue;
-    int PWM10BitValue;
     float period;
     unsigned long FOSC, _resolution = RESOLUTION;
-    
-    if(FPWM == frequencia_pwm){
-        SetDutyCyclePWM1(duty_cycle, (int) period);
-        return (EXIT_SUCCESS);
-    }
-    
-    frequencia_pwm = FPWM;
 
     if (FPWM < MINTHR)    {TimerPrescaleBits = 2; TimerPrescaleValue = Timer2Prescale_16;}
     else                  {TimerPrescaleBits = 0; TimerPrescaleValue = Timer2Prescale_1;}
@@ -77,58 +58,7 @@ int setPWM1(unsigned long FPWM, float duty_cycle){
     T2CON = TimerPrescaleBits;
     TMR2 = 0;
     T2CONbits.TMR2ON = 1;  /* Turn ON Timer2 */
-    
-    
-    PWM10BitValue = 4.0 * ((float)period + 1.0) * (duty_cycle/100.0);
-    CCPR1L = (PWM10BitValue >> 2);
-    CCP1CON = ((PWM10BitValue & 0x03) << 4) | 0x0C;
-    
-    SetDutyCyclePWM1(duty_cycle, (int) period);
-    return (EXIT_SUCCESS);
-}
-
-int setPWM2(unsigned long FPWM, float duty_cycle){
-    int clockSelectBits, TimerPrescaleBits;
-    int TimerPrescaleValue;
-    int PWM10BitValue;
-    float period;
-    unsigned long FOSC, _resolution = RESOLUTION;
-    
-    if(FPWM == frequencia_pwm){
-        SetDutyCyclePWM2(duty_cycle, (int) period);
-        return (EXIT_SUCCESS);
-    }
-    
-    frequencia_pwm = FPWM;
-
-    if (FPWM < MINTHR)    {TimerPrescaleBits = 2; TimerPrescaleValue = Timer2Prescale_16;}
-    else                  {TimerPrescaleBits = 0; TimerPrescaleValue = Timer2Prescale_1;}
-
-    if (FPWM > _resolution)               {clockSelectBits = 7; FOSC = InternalOsc_8MHz;}
-    else if (FPWM > (_resolution >>= 1))  {clockSelectBits = 6; FOSC = InternalOsc_4MHz;}
-    else if (FPWM > (_resolution >>= 1))  {clockSelectBits = 5; FOSC = InternalOsc_2MHz;}
-    else if (FPWM > (_resolution >>= 1))  {clockSelectBits = 4; FOSC = InternalOsc_1MHz;}
-    else if (FPWM > (_resolution >>= 1))  {clockSelectBits = 3; FOSC = InternalOsc_500KHz;}
-    else if (FPWM > (_resolution >>= 1))  {clockSelectBits = 2; FOSC = InternalOsc_250KHz;}
-    else if (FPWM > (_resolution >>= 1))  {clockSelectBits = 1; FOSC = InternalOsc_125KHz;}
-    else                                  {clockSelectBits = 0; FOSC = InternalOsc_31KHz;}
-
-    period = ((float)FOSC / (4.0 * (float)TimerPrescaleValue * (float)FPWM)) - 1.0;
-    period = round(period);
-
-    OSCCON = ((clockSelectBits & 0x07) << 4) | 0x02;
-    PR2 = (int)period;
-    T2CON = TimerPrescaleBits;
-    TMR2 = 0;
-    T2CONbits.TMR2ON = 1;  /* Turn ON Timer2 */
-    
-    
-    PWM10BitValue = 4.0 * ((float)period + 1.0) * (duty_cycle/100.0);
-    CCPR1L = (PWM10BitValue >> 2);
-    CCP1CON = ((PWM10BitValue & 0x03) << 4) | 0x0C;
-    
-    SetDutyCyclePWM2(duty_cycle, (int) period);
-    return (EXIT_SUCCESS);
+    return (int)period;
 }
 
 void SetDutyCyclePWM1(float Duty_cycle, int Period){
@@ -147,4 +77,4 @@ void SetDutyCyclePWM2(float Duty_cycle, int Period){
     CCP2CON = ((PWM10BitValue & 0x03) << 4) | 0x0C;
 }
 
-#endif	/* PWM_H */
+#endif  /* PWM_H */
